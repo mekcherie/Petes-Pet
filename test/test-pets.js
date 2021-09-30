f9a7d5bb73b6de0aee8f780dca156d8416aefe06
Meklit Cherie <makywonde347@yahoo.com>
2021-09-29 20:04:27 -0700

    users can search pets by name and breed

--- test/test-pets.js
+++ test/test-pets.js
@@ -116,4 +116,14 @@ describe('Pets', ()  => {
       });
     });
   });
+  // SEARCH
+  it('should search ALL pets by name on /search GET', (done) => {
+    chai.request(server)
+        .get('/search?term=norman')
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.should.be.html;
+          done();
+        });
+  });
 });
